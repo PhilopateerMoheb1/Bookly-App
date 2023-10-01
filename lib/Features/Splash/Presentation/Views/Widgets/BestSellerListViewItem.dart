c102a73a5c4b4c189fd82fa819241b84c577169a
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-10-01 08:57:17 +0300

    Feat:App Stykes

--- lib/Features/Splash/Presentation/Views/Widgets/BestSellerListViewItem.dart
+++ lib/Features/Splash/Presentation/Views/Widgets/BestSellerListViewItem.dart
@@ -1,37 +1,6 @@
-import 'package:booklyapp/Core/utils/Styles.dart';
-import 'package:booklyapp/Core/utils/assets.dart';
-import 'package:booklyapp/Features/Splash/Presentation/Views/Widgets/FeaturedListView.dart';
 import 'package:flutter/material.dart';
 
-import 'CustomAppBar.dart';
-
-class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedListView(),
-        SizedBox(
-          height: 30,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 30,
-          ),
-          child: Text(
-            "Best Seller",
-            style: Styles.textStyle18,
-          ),
-        ),
-        BestSellerListViewItem(),
-      ],
-    );
-  }
-}
+import '../../../../../Core/utils/assets.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
